bf17b4d04d88d42e9d9fc0ba994e268e0f3f9ef9
Joey Nichols <nichols@bermanco.com>
2016-12-06 11:18:21 -0500

    Removing posts from nav menu interface

--- setup/admin.php
+++ setup/admin.php
@@ -6,17 +6,17 @@
 /**
  * Modify the admin menu (left hand navigation)
  */
-function modify_admin_menu(){
+function bac_modify_admin_menu(){
 	// Remove the "Posts" menu item
 	remove_menu_page('edit.php');
 }
 
-add_action('admin_menu', 'modify_admin_menu');
+add_action('admin_menu', 'bac_modify_admin_menu');
 
 /**
  * Modify the admin bar (top of page navigation)
  */
-function modify_admin_bar(){
+function bac_modify_admin_bar(){
 	global $wp_admin_bar;
 
 	/* Update the functionality of the default "New" node */
@@ -28,4 +28,27 @@ function modify_admin_bar(){
 	$wp_admin_bar->remove_menu('new-post');
 }
 
-add_action('admin_bar_menu', 'modify_admin_bar', 80);
+add_action('admin_bar_menu', 'bac_modify_admin_bar', 80);
+
+/**
+ * Modify the Dashboard
+ */
+function bac_modify_dashboard(){
+	// Remove the "Quick Draft" widget
+	remove_meta_box('dashboard_quick_press','dashboard','side');
+	// Remove the "WordPress News" widget
+	remove_meta_box('dashboard_primary','dashboard','side');
+}
+
+add_action('wp_dashboard_setup', 'bac_modify_dashboard');
+
+/**
+ * Modify standard "Posts" post type
+ */
+function bac_modify_posts($args, $post_type){
+	if ('post' == $post_type){
+		$args['show_in_nav_menus'] = false;
+	}
+	return $args;
+}
+add_filter('register_post_type_args', 'bac_modify_posts', 10, 2);
